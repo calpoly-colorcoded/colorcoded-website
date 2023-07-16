ad1077dfc5610ed5adef7aecf9c4f08f82b70345
David Hernandez <76022084+davidh167@users.noreply.github.com>
2023-07-16 15:55:50 -0700

    Added Skeleton.dev css styling
    Added new Svelte UI library to project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/about">About Us</a>
+		</li>
+	</ul>
+</nav>
+
+<slot />
